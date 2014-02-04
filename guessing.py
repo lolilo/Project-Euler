020a5b169e9a467fdaf45fd7e35f3d61f477f6cb
Hackbright Student <user@chromebox-003.(none)>
2014-02-04 12:00:44 -0800

    Generate random number

--- guessing.py
+++ guessing.py
@@ -1,3 +1,13 @@
+import random
+
 print "Hello! What's your name?"
 name = raw_input("> ")
 print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." %name
+
+answer = random.randint(0,101)
+
+print answer
+
+while True:
+    print "Your guess?"
+    guess = raw_input("> ")
